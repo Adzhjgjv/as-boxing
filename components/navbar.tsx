cb213706cc8d179a5e6797583493c19460b07186
Adzhjgjv <137365550+Adzhjgjv@users.noreply.github.com>
2026-05-07 13:49:29 +0100

    Increase logo height in navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -48,7 +48,7 @@ export function Navbar() {
               alt="AS Boxing & Fitness"
               width={140}
               height={120}
-              className="h-16 w-auto"
+              className="h-20 w-auto"
               priority
             />
           </Link>
