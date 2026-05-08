fccd3c8ac219d409a88726d40629ac145bc90f47
Adzhjgjv <137365550+Adzhjgjv@users.noreply.github.com>
2026-05-08 19:55:20 +0100

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ export function Hero() {
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0">
-     <video
+<video
   autoPlay
   muted
   loop
